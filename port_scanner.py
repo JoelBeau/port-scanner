df49f888f31b9b67bff121014697e7519ac4489b
JoelBeau <joelbravo@protonmail.com>
2025-03-28 19:23:50 -0500

    Implemeneted parrallel port scanning for tcp_connect

--- port_scanner.py
+++ port_scanner.py
@@ -1,5 +1,8 @@
 import socket
+import threading
 from port import Port
+
+lock = threading.Lock()
 
 def tcp_connect_scan(port_list: list[Port], host: str, port: int):
 
@@ -16,13 +19,21 @@ def tcp_connect_scan(port_list: list[Port], host: str, port: int):
     finally:
         s.close()
 
-    port_list.append(Port(host, port, is_open))
+    # With the mutex lock, append the port to the port list
+    with lock:
+        port_list.append(Port(host, port, is_open))
 
 port_list: list[Port] = []
+scanning_threads: list[threading.Thread] = []
 
 for p in range(0, 50):
-    print(f"Scanning port: {p}")
-    tcp_connect_scan(port_list, '127.0.0.1', p)
+    thread = threading.Thread(target=tcp_connect_scan, args=(port_list, "127.0.0.1", p))
+    scanning_threads.append(thread)
+    thread.start()
+
+for t in scanning_threads:
+    t.join()
+
 
 open_ports = [p if p.check() else None for p in port_list]
 port_list.sort(key=lambda x: x.get_port())
