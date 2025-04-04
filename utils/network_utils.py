31d6cfc82f62b8a0d76c6c8e2d82bb8f3a5db81f
JoelBeau <joelbravo@protonmail.com>
2025-04-04 00:15:42 -0500

    Added function to get banner on open port

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -15,3 +15,15 @@ def get_host_mac(ip):
 def check_ip(ip):
     output = os.popen(f"ping {ip} -c 4").read()
     return False if "not known" in output else True
+
+# Gets banner service on open port
+def get_banner(ip, port):
+    try:
+        socket.setdefaulttimeout(2)
+        s = socket.socket()
+        s.connect((ip, port))
+        s.send(b'GET / HTTP/1.1\r\nHost: ' + ip.encode("utf-8") + b'\r\n\r\n')
+        return s.recv(1024).decode("utf-8")
+    except:
+        return None
+
