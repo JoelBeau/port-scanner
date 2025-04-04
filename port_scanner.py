9df9e9e51183e32eb7856bc4b3af1f574942a78c
JoelBeau <joelbravo@protonmail.com>
2025-04-04 18:15:06 -0500

    Updated port_scanner script for testing, still testing

--- port_scanner.py
+++ port_scanner.py
@@ -1,60 +1,60 @@
 import os
 import argparse
 
-import socket
 import threading
 import ipaddress as ipa
 
 from utils.models import Port, Arguements
-from utils.network_utils import get_host_mac
 from utils.scans import TCPConnect, SYNScan
-from scapy.all import sr1
-from scapy.layers.inet import ICMP, IP, TCP, Ether
+
 
 
 MAX_THREADS = 4799
 
-# Initialize arguments class and get the cli arguements
+# # Initialize arguments class and get the cli arguements
 flags = Arguements().args
+
+# r = flags['target']
+# e = flags["exclude"]
 
 print(flags)
 
-# Test arguements ensuring it works
-r = flags['target']
-e = flags["exclude"]
-type = flags["scan_type"]
+# # Test arguements ensuring it works
+# type = flags["scan_type"]
 
-print(r)
-print(e)
-print(type)
+# print(r)
+# print(e)
+# print(type)
 
-# Ensure's ip range is right and the exclusions are right
-for ip in r:
-    if ip not in e:
-        print(ipa.IPv4Address(ip))
+# # Ensure's ip range is right and the exclusions are right
+# for ip in r:
+#     if ip not in e:
+#         print(ipa.IPv4Address(ip))
 
-port_list: list[Port] = []
+# port_list: list[Port] = []
 
-scanning_threads: list[threading.Thread] = []
+# scanning_threads: list[threading.Thread] = []
 
-cse3320_ip = socket.gethostbyname("cse3320.org")
+# cse3320_ip = socket.gethostbyname("cse3320.org")
 
-tcp_scan = TCPConnect(cse3320_ip).scan
-syn_scan = SYNScan(cse3320_ip).scan
+# tcp_scan = TCPConnect(cse3320_ip).scan
+# syn_scan = SYNScan(cse3320_ip).scan
 
-# Test the first 50 ports
-for p in range(1, 50):
-    thread = threading.Thread(
-        target=syn_scan, args=(port_list, str(cse3320_ip), p, 5)
-    )
-    scanning_threads.append(thread)
-    thread.start()
+# syn_scan(port_list, 80, timeout=2, retry=3, verbose=True)
 
-for t in scanning_threads:
-    t.join()
+# # Test the first 50 ports
+# for p in range(1, 50):
+#     thread = threading.Thread(
+#         target=syn_scan, args=(port_list, str(cse3320_ip), p, 5)
+#     )
+#     scanning_threads.append(thread)
+#     thread.start()
 
-port_list.sort(key=lambda x: x.get_port())
+# for t in scanning_threads:
+#     t.join()
 
-for p in port_list:
-    # if p.check():
-    print(p)
+# port_list.sort(key=lambda x: x.get_port())
+
+# for p in port_list:
+#     # if p.check():
+#     print(p)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from scapy.all import IP, TCP, sr1, Ether
 from scapy.layers.inet import ICMP
 import socket
 import os
+import requests
 # Test using scapy package
 
 # Get mac Address of given ipaddress
@@ -13,12 +14,14 @@ def get_banner(ip, port):
         socket.setdefaulttimeout(2)
         s = socket.socket()
         s.connect((ip, port))
-        s.send(b'GET / HTTP/1.1\r\nHost: ' + ip.encode("utf-8") + b'\r\n\r\n')
-        return s.recv(1024).decode("utf-8")
+        banner = s.recv(1024).decode("utf-8")
+        return banner
     except:
         return None
+    finally:
+        s.close()
 
-port = 22
+port = 80
 
 cse3320_ip = socket.gethostbyname("cse3320.org")
 
@@ -26,7 +29,7 @@ cse3320_ip = socket.gethostbyname("cse3320.org")
 
 # eth_layer = Ether(dst="f2:3c:91:37:3a:7b".upper())
 
-ip_layer =  IP(dst=cse3320_ip)
+ip_layer =  IP(dst="127.0.0.1")
 tcp_layer = TCP(dport=port, flags="S", sport=12345, seq=1000)
 
 packet = ip_layer / tcp_layer
@@ -35,11 +38,30 @@ response = sr1(packet, timeout=2, verbose=0)
 print(response.show())
  
 if response and response.haslayer(TCP) and response[TCP].flags == "SA":
-    banner = get_banner(cse3320_ip, port).strip()
-    print("Banner: ", banner)
+    # banner = get_banner(cse3320_ip, port).strip()
+    # print("Banner: ", banner)
     print("Received SYN/ACK!")
 else:
     print("No SYN/ACK received.")
 
-mac = os.popen("curl -s ifconfig.me | arp -n | grep :").read()
-print(get_host_mac())
+# mac = os.popen("curl -s ifconfig.me | arp -n | grep :").read()
+# print(get_host_mac())
+
+user_agent = "Mozilla/5.0"
+
+port = 80
+
+try:
+    protocol = "http" if port == 80 else "https"
+    url = f"{protocol}://127.0.0.1:{80}/"
+
+    headers = {}
+
+    if user_agent:
+        headers["User-Agent"] = user_agent
+
+    response = requests.get(url, headers=headers, timeout=2)
+
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print(f"Error: {e}")
