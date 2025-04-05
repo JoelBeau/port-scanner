3bf3a7de63983e5d564a60c63c967161045164dc
Joel Beauregard <joelbravo@protonmail.com>
2025-04-05 15:17:35 -0500

    Updated output function for printing output

--- utils/scanner_utils.py
+++ utils/scanner_utils.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import pandas
 from models import Port
 
 # Get ip of specified host
@@ -32,7 +33,18 @@ def get_banner(ip: str, port: int, user_agent=False):
     return banner if banner else "No service to be found on open port"
 
 # Skeleton for outputing results
-def output(port_list: list[Port], format="text"):
-    pass
+def output(port_list: list[Port], format):
+    
+    if format == "text":
+        pd = pandas.DataFrame()
+
+        for p in port_list:
+
+            pd["host-ip"] = p.get_host()
+            pd["port-tested"] = p.get_port()
+            pd["port-status"] = p.get_status()
+            pd["port-is-open"] = p.check()
+            pd["port-banner"] = p.get_banner()
+        
     
 
