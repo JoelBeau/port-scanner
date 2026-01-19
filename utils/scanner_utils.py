18f1097912bf04e9468c1423c9354583838f98c5
JoelBeau <joelbravo@protonmail.com>
2026-01-19 05:53:55 +0000

    Wrote a new function to handle multiple host ~90% completed

--- utils/scanner_utils.py
+++ utils/scanner_utils.py
@@ -4,20 +4,63 @@ import io
 import csv
 import json
 
-from scapy.layers.inet import Ether
+import ipaddress as ipa
+import threading
 
 from tabulate import tabulate
 from .models import Port
-
+from utils.conf import logger
+from utils import conf
+from utils.scans import TCPConnect, SYNScan
 
 # Get ip of specified host
 def get_ip(host: str):
     return socket.gethostbyname(host)
 
 # Ensure ip is reachable
-def check_ip(ip: str):
+def is_reachable(ip: str):
     output = os.popen(f"ping {ip} -c 4").read()
     return False if "not known" in output else True
+
+def scan_multiple_hosts(**flags):
+    hosts = flags["target"]
+    scan_type = flags["scan_type"]
+    exclusions = flags["exclude"]
+    verbosity = flags["verbose"]
+
+    scanning_threads: list[threading.Thread] = []
+
+    for host in hosts:
+        port_list: list[Port] = []
+
+        if type(hosts) is not ipa.IPv4Network:
+            host = ipa.IPv4Address(host)
+        if not is_reachable(host):
+            logger.warning(f"Host {host} is not reachable, skipping...")
+            continue
+        if host in exclusions:
+            logger_message = f"Host {host} is in exclusions, skipping..."
+            if verbosity >= conf.MINIMUM_VERBOSITY:
+                print(logger_message)
+            logger.info(logger_message)
+            continue
+
+        args = set()
+        if scan_type == "tcp":
+            pscanner = TCPConnect(str(host), flags)
+        else:
+            pscanner = SYNScan(str(host), flags)
+
+        t = threading.Thread(target=pscanner.scan_host, args=args)
+        scanning_threads.append((t, port_list, pscanner))
+        t.start()
+
+    for t, port_list, pscanner in scanning_threads:
+        t.join()
+        output(port_list, pscanner.output_medium)
+
+
+
 
 # Skeleton for outputing results
 def output(port_list: list[Port], medium: tuple):
@@ -28,7 +71,7 @@ def output(port_list: list[Port], medium: tuple):
 
         data = list(map(lambda p: list(p), port_list))
 
-        headers = ["Host IP", "Port Test", "Port Status", "Port Is Open", "Port Banner"]
+        headers = ["Host IP", "Port Tested", "Port Status", "Port Is Open", "Port Banner"]
 
         results = tabulate(data, headers=headers, tablefmt="grid")
 
