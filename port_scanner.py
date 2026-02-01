2e7ee3c8235c12e8cf527acd3922304533a18db5
JoelBeau <joelbravo@protonmail.com>
2026-02-01 02:06:46 +0000

    Updated main file, removed junk

--- port_scanner.py
+++ port_scanner.py
@@ -7,6 +7,7 @@ from utils.scanner_utils import scan
 
 
 def main():
+
     """Main entry point for the port scanner."""
     flags = Arguments().args
     
@@ -16,10 +17,9 @@ def main():
     asyncio.run(scan(**flags))
     
     end = time.time()
-    
+
     logger.info("Port scanning completed.")
     logger.info(f"Total scanning time: {end - start:.2f} seconds")
-
 
 if __name__ == "__main__":
     main()
