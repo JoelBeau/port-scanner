340f1e2c2f8f84ec2b10c38653e5f557dfe499b7
JoelBeau <joelbravo@protonmail.com>
2026-02-01 03:53:35 +0000

    Moved validation functions to validation.py in utils

--- port_scanner/models/arguments.py
+++ port_scanner/models/arguments.py
@@ -1,9 +1,8 @@
 import argparse
-import socket
 import sys
-import utils.errors as errors
-import ipaddress as ipa
+
 import config as conf
+from utils import validation
 
 
 class ArgParser(argparse.ArgumentParser):
@@ -19,22 +18,22 @@ class Arguments:
         self.parser = ArgParser(
             description="Port Scanner - A tool for scanning ports and retrieving banners."
         )
-        self.args = self.get_flags()
+        self.args = self._get_flags()
 
     # Parse flags with arguement parser
-    def get_flags(self):
+    def _get_flags(self):
 
         self.parser.add_argument(
             "-t",
             "--target",
-            type=self.parse_ips,
+            type=validation.parse_ips,
             required=True,
             help="Specify the target IP or range of IPs to scan",
         )
         self.parser.add_argument(
             "-p",
             "--port",
-            type=self.parse_port_range,
+            type=validation.parse_port_range,
             default=conf.DEFAULT_PORT_RANGE,
             help="Specify the range of ports to scan (e.g., 1-1024 or 80)",
         )
@@ -56,7 +55,7 @@ class Arguments:
         self.parser.add_argument(
             "-o",
             "--output",
-            type=self.parse_outputs,
+            type=validation.parse_outputs,
             default=conf.DEFAULT_OUTPUT_MEDIUM,
             help="Specify the output format or file",
         )
@@ -84,7 +83,7 @@ class Arguments:
         self.parser.add_argument(
             "-e",
             "--exclude",
-            type=self.parse_exclusions,
+            type=validation.parse_exclusions,
             help="Exclude specific IPs or ports from the scan",
         )
         self.parser.add_argument(
@@ -98,90 +97,3 @@ class Arguments:
         args = self.parser.parse_args()
 
         return vars(args)
-
-    def parse_outputs(self, output):
-
-        # If outputting to a file
-        if "." in output:
-            return (output, conf.OUTPUT_TO_FILE)
-
-        # If just outputting to the terminal
-        if output in conf.FORMAT_TYPES:
-            return (output, conf.OUTPUT_TO_CONSOLE)
-        
-        raise errors.InvalidOutputFormatError(output)
-
-    def parse_exclusions(self, value: str):
-        if "," not in value:
-            return [self.validate_exclusions(value.strip())]
-        else:
-            exclusions = value.split(",")
-            modified_exclusions = []
-            for e in exclusions:
-                modified_exclusions.append(self.validate_exclusions(e.strip()))
-            return modified_exclusions
-
-    def validate_exclusions(self, value: str):
-        try:
-            if "." in value:
-                ip = ipa.IPv4Address(value)
-                return int(ip)
-            else:
-                p = int(value)
-                if p > conf.MAXIMUM_PORT or p == conf.PORT_NOT_ALLOWED:
-                    raise ValueError
-                else:
-                    return p
-        except ValueError:
-            raise errors.InvalidPortExclusionError(value)
-        except ipa.AddressValueError:
-            raise errors.InvalidIPExclusionError(value)
-
-    def parse_ips(self, ips: str):
-        # Check if it's CIDR notation
-        if "/" in ips:
-            try:
-                network = ipa.IPv4Network(ips, strict=False)
-            except ipa.NetmaskValueError:
-                raise errors.InvalidCIDRError(ips)
-            return list(network)
-        # Not in CIDR notation
-        elif "-" not in ips:
-            try:
-                int(ips)
-                raise errors.InvalidIPError(ips)
-            except ValueError:
-                try:
-                    ip = ipa.IPv4Address(ips)
-                except ipa.AddressValueError:
-                    try:
-                        resolved_ip = ipa.IPv4Address(socket.gethostbyname(ips))
-                        return resolved_ip
-                    except socket.gaierror:
-                        raise errors.InvalidIPError(ips)
-            return ips
-        else:
-            try:
-                ips = ips.split("-")
-                start = int(ipa.IPv4Address(ips[0]))
-                end = int(ipa.IPv4Address(ips[1]))
-                # Make sure each address is a valid address
-                for ip in range(start, end):
-                    ip = ipa.IPv4Address(ip)
-                return range(start, end)
-            except ipa.AddressValueError:
-                raise errors.InvalidIPRangeError(ips)
-
-    def parse_port_range(self, ports: str):
-        delim = "," if "," in ports else "-" if "-" in ports else None
-        
-        if not delim:
-            port = int(ports)
-            if port < conf.MINIMUM_PORT or port > conf.MAXIMUM_PORT:
-                raise errors.InvalidPortError(ports)
-            return port
-        else:
-            start, end = map(int, ports.split(delim))
-            if start > end or start == conf.PORT_NOT_ALLOWED or end > conf.MAXIMUM_PORT:
-                raise errors.InvalidPortRangeError(ports)
-            return range(start, end + 1)
---
+++ port_scanner/utils/validation.py
@@ -0,0 +1,93 @@
+import ipaddress as ipa
+import socket
+
+import config as conf
+import errors
+
+
+def parse_outputs(self, output):
+
+        # If outputting to a file
+        if "." in output:
+            return (output, conf.OUTPUT_TO_FILE)
+
+        # If just outputting to the terminal
+        if output in conf.FORMAT_TYPES:
+            return (output, conf.OUTPUT_TO_CONSOLE)
+        
+        raise errors.InvalidOutputFormatError(output)
+
+def parse_exclusions(self, value: str):
+    if "," not in value:
+        return [self.validate_exclusions(value.strip())]
+    else:
+        exclusions = value.split(",")
+        modified_exclusions = []
+        for e in exclusions:
+            modified_exclusions.append(self.validate_exclusions(e.strip()))
+        return modified_exclusions
+
+def validate_exclusions(self, value: str):
+    try:
+        if "." in value:
+            ip = ipa.IPv4Address(value)
+            return int(ip)
+        else:
+            p = int(value)
+            if p > conf.MAXIMUM_PORT or p == conf.PORT_NOT_ALLOWED:
+                raise ValueError
+            else:
+                return p
+    except ValueError:
+        raise errors.InvalidPortExclusionError(value)
+    except ipa.AddressValueError:
+        raise errors.InvalidIPExclusionError(value)
+
+def parse_ips(self, ips: str):
+    # Check if it's CIDR notation
+    if "/" in ips:
+        try:
+            network = ipa.IPv4Network(ips, strict=False)
+        except ipa.NetmaskValueError:
+            raise errors.InvalidCIDRError(ips)
+        return list(network)
+    # Not in CIDR notation
+    elif "-" not in ips:
+        try:
+            int(ips)
+            raise errors.InvalidIPError(ips)
+        except ValueError:
+            try:
+                ip = ipa.IPv4Address(ips)
+            except ipa.AddressValueError:
+                try:
+                    resolved_ip = ipa.IPv4Address(socket.gethostbyname(ips))
+                    return resolved_ip
+                except socket.gaierror:
+                    raise errors.InvalidIPError(ips)
+        return ips
+    else:
+        try:
+            ips = ips.split("-")
+            start = int(ipa.IPv4Address(ips[0]))
+            end = int(ipa.IPv4Address(ips[1]))
+            # Make sure each address is a valid address
+            for ip in range(start, end):
+                ip = ipa.IPv4Address(ip)
+            return range(start, end)
+        except ipa.AddressValueError:
+            raise errors.InvalidIPRangeError(ips)
+
+def parse_port_range(self, ports: str):
+    delim = "," if "," in ports else "-" if "-" in ports else None
+    
+    if not delim:
+        port = int(ports)
+        if port < conf.MINIMUM_PORT or port > conf.MAXIMUM_PORT:
+            raise errors.InvalidPortError(ports)
+        return port
+    else:
+        start, end = map(int, ports.split(delim))
+        if start > end or start == conf.PORT_NOT_ALLOWED or end > conf.MAXIMUM_PORT:
+            raise errors.InvalidPortRangeError(ports)
+        return range(start, end + 1)
