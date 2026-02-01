116afba9cf83e04998f8008d6b2d27c4f09c494a
JoelBeau <joelbravo@protonmail.com>
2026-02-01 02:06:17 +0000

    Updated logger to have a try/except block when attempting to create log file

--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,29 @@
 import logging
+import os
+import atexit
 
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-def setup_logger(name: str, log_file: str, level=logging.INFO) -> logging.Logger:
-    handler = logging.FileHandler(log_file)        
-    handler.setFormatter(logging.Formatter(LOG_FORMAT))
-
+def setup_logger(name: str, level=logging.INFO) -> logging.Logger:
+    log_dir = "/var/log/port-scanner/"
+    
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logger.addHandler(handler)
+    
+    # Try to create file handler, exit if permission denied
+    try:
+        os.makedirs(log_dir, exist_ok=True)
+        log_path = os.path.join(log_dir, "port_scanner.log")
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+        logger.addHandler(file_handler)
+    except (PermissionError, OSError):
+        print(f"port scanner: You don't have privileges to run port scanner on this device.")
+        exit(1)
+
+    # Flush on exit
+    atexit.register(lambda: [h.flush() for h in logger.handlers])
     
     return logger
 
-# Create global logger instance
-logger = setup_logger("port_scanner", "port_scanner.log")
+logger = setup_logger("port_scanner")
