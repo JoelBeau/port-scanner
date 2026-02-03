09b1d98ecfabc20b73942a92ddd5fcfce31fd0f1
JoelBeau <joelbravo@protonmail.com>
2026-02-03 18:17:20 +0000

    Eddited __main__.py to catch PortScannerError and print user-friendly message.

--- port_scanner/__main__.py
+++ port_scanner/__main__.py
@@ -1,4 +1,18 @@
+import sys
+from port_scanner.errors import PortScannerError
 from port_scanner.cli import main
+from port_scanner import config as conf
+
+def run():
+    try:
+        main()
+    except PortScannerError as e:
+        print(e)
+        return conf.EXIT_FAILURE
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return conf.EXIT_FAILURE
+    return conf.EXIT_SUCCESS
 
 if __name__ == "__main__":
-    main()
+    sys.exit(run())
